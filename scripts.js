e2a3b3d2b42b599f710be82ed6dbf76000849287
peteroffner <151682974+peteroffner@users.noreply.github.com>
2023-11-23 20:31:37 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+// Login
 
+function login() {
+  const email = document.querySelector("input[name='email']").value;
+  const password = document.querySelector("input[name='password']").value;
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+  // Send request to server
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "login.php");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(`email=${email}&password=${password}`);
 
-};
+  // Handle response
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Login successful
+      document.location.href = "dashboard.php";
+    } else {
+      // Login failed
+      alert("Die Anmeldung ist fehlgeschlagen.");
+    }
+  };
+}
 
-const scrollreveal = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: false
-});
+// Click event handler for login button
 
-scrollreveal.reveal('.text', {delay:200, origin:'top'});
-scrollreveal.reveal('.logo-img', {delay:450, origin:'top'});
-scrollreveal.reveal('.platform-img', {delay:450, origin:'top'});
-scrollreveal.reveal('.about-img', {delay:450, origin:'top'});
-scrollreveal.reveal('.innovation-img', {delay:450, origin:'top'});
-scrollreveal.reveal('.contact-img', {delay:450, origin:'top'});
-scrollreveal.reveal('.copyright-img', {delay:450, origin:'top'});
-scrollreveal.reveal('.icons', {delay:500, origin:'left'});
-scrollreveal.reveal('.scroll-down', {delay:500, origin:'right'});
+document.querySelector("button").addEventListener("click", login);
