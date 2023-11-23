8121558a6ad8b85b375f64fecec748db5b2f6d41
peteroffner <151682974+peteroffner@users.noreply.github.com>
2023-11-23 20:34:54 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,8 @@
-// Login
-
-function login() {
-  const email = document.querySelector("input[name='email']").value;
-  const password = document.querySelector("input[name='password']").value;
-
-  // Send request to server
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "login.php");
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.send(`email=${email}&password=${password}`);
-
-  // Handle response
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Login successful
-      document.location.href = "dashboard.php";
-    } else {
-      // Login failed
-      alert("Die Anmeldung ist fehlgeschlagen.");
-    }
-  };
-}
-
-// Click event handler for login button
-
-document.querySelector("button").addEventListener("click", login);
+// Laden der Ladestationen
+function loadLadestationen() {
+  // Ajax-Anfrage an den Server
+  fetch("/api/ladestationen")
+    .then(function(response) {
+      // Erfolgreiche Antwort
+      if (response.ok) {
+        // Ladestationen aus der Antwort in ein Array einlesen
